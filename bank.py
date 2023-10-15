c8a4d2bebd5356165f627b30113478f9a33d70db
Usman Virk <usmanvirk11@gmail.com>
2023-10-15 15:18:35 +0200

    Final Commit

--- bank.py
+++ bank.py
@@ -1,12 +1,6 @@
 greeting = input("Greetings :  ")
-
-# print (greeting)
-
 greeting = greeting.strip()
 greeting = greeting.lower()
-
-# print (greeting)
-
 
 if "hello" in greeting:
     print("$0")
@@ -14,3 +8,5 @@ elif greeting[0] == "h":
     print("$20")
 else:
     print("$100")
+
+    # End of program.
